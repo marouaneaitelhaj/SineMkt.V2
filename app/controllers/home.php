d99ad6a03fe08f1fc0d51c190c99af93d4f0d2a1
marouaneaitelhaj <112936725+marouaneaitelhaj@users.noreply.github.com>
2022-12-02 23:24:39 +0100

    update

---
+++ app/controllers/home.php
@@ -0,0 +1,6 @@
+<?php
+class home{
+    public function login(){
+
+    }
+}
--- app/core/app.php
+++ app/core/app.php
@@ -1,20 +1,29 @@
 <?php
-class app{
+class app
+{
+    protected $controller = 'home';
+    protected $method = 'index';
     public function __construct()
     {
+        if (isset($_GET['url'])) {
         $arr = $this->parseUrl();
-            if (file_exists('..app/controllers/' . $arr[0] . '.php')){
-                header('location: https://open.spotify.com/playlist/37i9dQZF1F0sijgNaJdgit');
+    
+            if (file_exists('../app/controllers/' . $arr[0] . '.php')) {
+                $this->controller = '../app/controllers/' . $arr[0] . '.php';
+                unset($arr[0]);
+            }
+            include($this->controller);
+            if (isset($arr[0])) {
+                if(method_exists())
             }
     }
+}
     public function parseUrl()
     {
-        if(isset($_GET['url']))
-        {
+        if (isset($_GET['url'])) {
             //return $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
             // echo $_GET['url'];
             return $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
         }
     }
 }
-?>
