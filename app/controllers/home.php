43bf58425f011e341fc0bfb9c13f9cf30c29194a
marouaneaitelhaj <112936725+marouaneaitelhaj@users.noreply.github.com>
2022-12-05 23:15:52 +0100

    update

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -2,11 +2,14 @@
 
 class home extends controller
 {
+    
     public function index($name)
     {
         $user = $this->model('User');
+        if(isset($name[0])){
         $user->name = $name[0];
         echo $user->name;
         $this->view('home/view', ['name' => $user->name]);
+    }
     }
 }
